d71028c4a325e3eec2f1e0b0986864b05719cdb8
Jonah <jonahwert07@gmail.com>
2026-02-05 11:47:34 -0500

    Merge remote-tracking branch 'origin/main'

--- src/monsters/Water.java
+++ src/monsters/Water.java
@@ -2,11 +2,12 @@ package monsters;
 
 public class Water extends Monster {
 
-        public Fire(String name, Eyes eyes) {
-            super(name, eyes);
-        }
-
-        @Override
-        public void specialPowers(){
-            System.out.println("sprays water")
+    public Fire(String name, Eyes eyes) {
+        super(name, eyes);
     }
+
+    @Override
+    public void specialPowers() {
+        System.out.println("sprays water")
+    }
+}
