4d62d2a143351abc3719aa22ca186b6d1ac040f7
Jonah <jonahwert07@gmail.com>
2026-02-07 14:46:27 -0500

    Merge remote-tracking branch 'origin/main'

--- src/monsters/Main.java
+++ src/monsters/Main.java
@@ -7,15 +7,18 @@ public class Main {
         Monster p2 = new Fire("Charizard", Eyes.ONE);
         Monster p3 = new Slime("Pikachu", Eyes.THREE);
         Monster p4 = new Sand("Pichu", Eyes.ONE);
+        Monster p5 = new Ink("Squirtle", Eyes.TWO);
 
         System.out.println("Pokemon 1 " + p1.getName());
         System.out.println("Pokemon 2 " + p2.getName());
         System.out.println("Pokemon 3 " + p3.getName());
         System.out.println("Pokemon 4 " + p4.getName());
+        System.out.println("Pokemon 5 " + p5.getName());
 
         p1.specialPowers();
         p2.specialPowers();
         p3.specialPowers();
         p4.specialPowers();
+        p5.specialPowers();
     }
 }
