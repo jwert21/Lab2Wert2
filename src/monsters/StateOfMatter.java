6adb4ce9f8a33d0a7ed3a8dea3352b7c098ef492
Jonah <jonahwert07@gmail.com>
2026-02-08 22:37:42 -0500

    Merge remote-tracking branch 'origin/main'

--- src/monsters/StateOfMatter.java
+++ src/monsters/StateOfMatter.java
@@ -1,3 +1,13 @@
+/** Project: Lab 2
+ * Purpose Details: Monsters
+ * Course: IST 242
+ * Author: Jonah Wert
+ * Date Developed: 2/5/2026
+ * Last Date Changed: 2/8/2026
+ * Rev: 2/8/2026
+
+ */
+
 package monsters;
 
 public enum StateOfMatter {
